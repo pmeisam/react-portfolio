e40d73cdcb53447130c51e8e00926a9ea65b13f1
Meisam Poorzand <mpoorzand@avtr.com>
2019-09-27 09:56:02 -0700

    adds animation to projects page

--- src/Pages/ProjectPage/ProjectPage.jsx
+++ src/Pages/ProjectPage/ProjectPage.jsx
@@ -1,20 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import Fade from 'react-reveal/Fade';
+import styled from 'styled-components';
 import Minesweeper from '../../Components/Minesweeper/Minesweeper';
 import Memesagram from '../../Components/Memesagram/Memesagram';
 import Triplaner from '../../Components/Triplaner/Triplaner';
 import DevSpot from '../../Components/DevSpot/DevSpot';
 
+const Project = styled.div`
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        `;
+
 class ProjectPage extends Component {
     
     render () {
+        
         return (
-            <>
-                <h1>Projects</h1>
-                <Minesweeper />
-                <Memesagram />
-                <Triplaner />
-                <DevSpot />
-            </>
+            
+        <>
+            <Project>
+                <Fade left><DevSpot /></Fade>
+            </Project>
+            <Project>
+                <Fade right><Triplaner /></Fade>
+            </Project>
+            <Project>
+                <Fade left><Memesagram /></Fade>
+            </Project>
+            <Project>
+                <Fade right><Minesweeper /><img src="https://cdn-ds.com/media/dfmodels/2506/AdditionalImage-73096.jpg" alt=""/></Fade>
+            </Project>
+        </>
+   
         )
     }
 }
--- src/Pages/SkillsPage/SkillsPage.jsx
+++ src/Pages/SkillsPage/SkillsPage.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import styled, {keyframes} from 'styled-components';
-import { slideInRight } from 'react-animations';
+import Fade from 'react-reveal/Fade';
 import './SkillsPage.scss';
 
 class SkillsPage extends Component {
@@ -9,10 +8,17 @@ class SkillsPage extends Component {
     render () {
       
         return (
-
-            <div className="aboutDiv">
-                SKILLS
-            </div>
+            <React.Fragment>
+                <div className="aboutDiv">
+                    <Fade top>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque et atque odio quam numquam ipsum, facere similique impedit amet consequatur officia? Tenetur vitae minima ducimus ex assumenda! Unde, excepturi.</Fade>
+                </div>
+                <div className="aboutDiv">
+                    <Fade center>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum vero natus quis eligendi architecto earum. Suscipit, modi! Ipsam asperiores temporibus commodi nisi voluptates, beatae eum, explicabo accusantium architecto iure quod!</Fade>
+                </div>
+                <div className="aboutDiv">
+                    <Fade center>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi fuga quos ab sint quae voluptates! Debitis repellendus ipsam itaque quis vero obcaecati laborum unde officia dolorem. Repellat aspernatur dignissimos sequi.</Fade>
+                </div>
+            </React.Fragment>
         )
     }
 }
