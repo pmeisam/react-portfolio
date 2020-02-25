2095b5b61bd705fd40b89b3a2c7640f1efd1b93c
Meisam Poorzand <mpoorzand@avtr.com>
2020-02-25 10:13:16 -0800

    Updates

---
+++ precache-manifest.2f95a828084b3c1350cb33a51abedea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baafda19a59c305c3546939a9368cc6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4338fba664c1c5cf6b",
+    "url": "/static/css/main.cb3b5b48.chunk.css"
+  },
+  {
+    "revision": "09a9a754098a06e9dabc",
+    "url": "/static/js/2.16b1d6e4.chunk.js"
+  },
+  {
+    "revision": "8f4338fba664c1c5cf6b",
+    "url": "/static/js/main.44b1240c.chunk.js"
+  },
+  {
+    "revision": "70327af8c84931932d34",
+    "url": "/static/js/runtime-main.98592f35.js"
+  }
+]);
