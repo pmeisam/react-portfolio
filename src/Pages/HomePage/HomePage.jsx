d9cb66ebe132fe16d8fa363ce8fc2fdd754d2135
Meisam Poorzand <pmeisam@ymail.com>
2020-03-29 15:08:28 -0700

    Revert "updates homepage flex"
    This reverts commit d5c0d2b0a910e7ce7721e31bc7ac97de15293793.

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -42,8 +42,7 @@ class HomePage extends Component {
     const Tada = styled.div`
       animation: 3s ${keyframes`${tada}`} 1;
       animation-delay: 0.5s;
-      width: 70vw;
-
+      margin-top: 150px;
       & > h1 {
         font-family: "Saira Stencil One", cursive;
         font-size: 4vw;
@@ -56,36 +55,34 @@ class HomePage extends Component {
       }
     `;
     const Intro = styled.div`
-      width: 100vw;
-      height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      & > h1 {
-          font-family: 'Saira Stencil One', cursive;
-          font-size: 2vw;
-          width: 70%;
-          height: 50vh;
-          margin: 0 auto;
-          .blinking{
-              animation:blinkingText 0.8s infinite;
-          }
-          @keyframes blinkingText{
-              0%{		color: ${this.props.theme.fontColor};	}
-              49%{	color: transparent;	}
-              50%{	color: transparent;	}
-              99%{	color:transparent;	}
-              100%{	color: ${this.props.theme.fontColor};	}
-          }
-      }
-      @media screen and (max-width: 600px) {
-          & > h1 {
-              font-size: 14px;
-          }
-      }
-     }
-    `;
+            width: 80%;
+            margin: 0 auto;
+            max-height: 90vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            & > h1 {
+                font-family: 'Saira Stencil One', cursive;
+                font-size: 2vw;
+                .blinking{
+                    animation:blinkingText 0.8s infinite;
+                }
+                @keyframes blinkingText{
+                    0%{		color: ${this.props.theme.fontColor};	}
+                    49%{	color: transparent;	}
+                    50%{	color: transparent;	}
+                    99%{	color:transparent;	}
+                    100%{	color: ${this.props.theme.fontColor};	}
+                }
+            }
+            @media screen and (max-width: 600px) {
+                & > h1 {
+                    font-size: 14px;
+                }
+            }
+
+        }
+        `;
     return (
       <Intro>
         <Tada>
