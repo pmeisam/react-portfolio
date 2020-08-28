74ce8f8812664297690057ae3bfa6816da83806c
Meisam Poorzand <pmeisam@ymail.com>
2020-08-28 00:55:15 -0700

    Updates

---
+++ precache-manifest.c420cd098fcb492f2a6a4f7d9ca8301b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da647c8f2309043dee9e6957bcf48c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1feedaf51592207367",
+    "url": "/static/css/main.8a620ffb.chunk.css"
+  },
+  {
+    "revision": "063df1b8374bcb9777fe",
+    "url": "/static/js/2.e3a5645a.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.e3a5645a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1feedaf51592207367",
+    "url": "/static/js/main.cc841ab3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
