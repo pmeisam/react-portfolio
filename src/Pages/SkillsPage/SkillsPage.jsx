d56e1381389dffaf90ea146ac13fd63f2a096355
Meisam Poorzand <pmeisam@ymail.com>
2020-09-18 19:32:26 -0700

    Revert "updates skills page"
    This reverts commit 3c424b5ebd9890e6fca147f1c44aba036ada3454.

--- src/Pages/SkillsPage/SkillsPage.jsx
+++ src/Pages/SkillsPage/SkillsPage.jsx
@@ -9,104 +9,56 @@ const SkillsPage = () => {
   return (
     <React.Fragment>
       <Fade top>
-        <div className="skills_parent">
-          <div className="skill_item">
-            <i className="devicon-nodejs-plain colored skillSize"></i>
-            <span className="skillSearch">nodejs</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-express-original skillSize"></i>
-            <span className="skillSearch">express</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-mongodb-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">MongoDB</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-angularjs-plain colored skillSize"></i>
-            <span className="skillSearch">Angular</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-react-original-wordmark colored skillSize"></i>
-            <span className="skillSearch">React</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-python-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">Python</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-django-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">Django</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-postgresql-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">PostgreSQL</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-typescript-plain colored skillSize"></i>
-            <span className="skillSearch">Typescript</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-javascript-plain colored skillSize"></i>
-            <span className="skillSearch">JavaScript</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-jquery-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">jquery</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-c-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">C</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-cplusplus-plain colored skillSize"></i>
-            <span className="skillSearch">Cplusplus</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-github-plain-wordmark skillSize"></i>
-            <span className="skillSearch">git</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-html5-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">HTML5</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-css3-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">CSS</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-java-plain-wordmark colored skillSizeplain"></i>
-            <span className="skillSearch">java</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-sass-original colored skillSize"></i>
-            <span className="skillSearch">sass scss</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-wordpress-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">wordpress</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-amazonwebservices-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">Amazon Web Service</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-bootstrap-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">Bootstrap</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-git-plain colored skillSize"></i>
-            <span className="skillSearch">github</span>
-            <span className="skillSearch">git</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-heroku-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">heroku</span>
-          </div>
-          <div className="skill_item">
-            <i className="devicon-mysql-plain-wordmark colored skillSize"></i>
-            <span className="skillSearch">mysql</span>
-          </div>
+        <div className="skills">
+          <i className="devicon-nodejs-plain colored skillSize"></i>
+          <span className="skillSearch">nodejs</span>
+          <i className="devicon-express-original skillSize"></i>
+          <span className="skillSearch">express</span>
+          <i className="devicon-mongodb-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">MongoDB</span>
+          <i className="devicon-angularjs-plain colored skillSize"></i>
+          <span className="skillSearch">Angular</span>
+          <i className="devicon-react-original-wordmark colored skillSize"></i>
+          <span className="skillSearch">React</span>
+          <i className="devicon-python-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">Python</span>
+          <i className="devicon-django-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">Django</span>
+          <i className="devicon-postgresql-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">PostgreSQL</span>
+          <i className="devicon-typescript-plain colored skillSize"></i>
+          <span className="skillSearch">Typescript</span>
+          <i className="devicon-javascript-plain colored skillSize"></i>
+          <span className="skillSearch">JavaScript</span>
+          <i className="devicon-jquery-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">jquery</span>
+          <i className="devicon-c-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">C</span>
+          <i className="devicon-cplusplus-plain colored skillSize"></i>
+          <span className="skillSearch">Cplusplus</span>
+          <i className="devicon-github-plain-wordmark skillSize"></i>
+          <span className="skillSearch">git</span>
+          <i className="devicon-html5-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">HTML5</span>
+          <i className="devicon-css3-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">CSS</span>
+          <i className="devicon-java-plain-wordmark colored skillSizeplain"></i>
+          <span className="skillSearch">java</span>
+          <i className="devicon-sass-original colored skillSize"></i>
+          <span className="skillSearch">sass scss</span>
+          <i className="devicon-wordpress-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">wordpress</span>
+          <i className="devicon-amazonwebservices-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">Amazon Web Service</span>
+          <i className="devicon-bootstrap-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">Bootstrap</span>
+          <span className="skillSearch">github</span>
+          <i className="devicon-git-plain colored skillSize"></i>
+          <span className="skillSearch">git</span>
+          <i className="devicon-heroku-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">heroku</span>
+          <i className="devicon-mysql-plain-wordmark colored skillSize"></i>
+          <span className="skillSearch">mysql</span>
         </div>
       </Fade>
     </React.Fragment>
